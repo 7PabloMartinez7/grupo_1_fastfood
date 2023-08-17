9fa83dd5706fa7cd1a15be9e572daf35363ef21b
gonzalotaritolay94@gmail.com <gonzalotaritolay94@gmail.com>
2023-08-16 21:31:48 -0300

    agregando mvc de index

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require ("express")
 const path = require ("path"); 
-
 const app= express();
-
 const publicPath = path.resolve (__dirname, "./public");
+
+const rutasIndex= require("./routes/index.js")
+//para que funciente ejs se usa app.set
+app.set("view engine", "ejs")
+
 app.use (express.static(publicPath));
+
+app.use("/", rutasIndex)
 
 app.listen(5000,() => {
     console.log ("Servidor corriendo en el puerto 5000");
  } );
 
-app.get("/",(req,res)=>{
+
+/*app.get("/",(req,res)=>{
    res.sendFile(path.join(__dirname,"./views/index.html"))
-})
+})*/
 
 app.get("/register",(req,res)=>{
    res.sendFile(path.resolve('./views/register.html'));
