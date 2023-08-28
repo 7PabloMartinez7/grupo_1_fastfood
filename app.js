410d3d811c509507fd89e0b1f01704c8512c6d46
gonzalotaritolay94@gmail.com <gonzalotaritolay94@gmail.com>
2023-08-28 10:45:47 -0300

    añadiendo json

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ("express")
 const path = require ("path"); 
 const app= express();
 const publicPath = path.resolve (__dirname, "./public");
-//Rutas
+//Rutas requeridas
 const Index= require("./routes/index.js")
 const product= require("./routes/product.js")
 const Novedades= require("./routes/novedades.js")
@@ -10,14 +10,14 @@ const registro= require("./routes/registro.js")
 //para que funciente ejs se usa app.set
 app.set("view engine", "ejs")
 app.use (express.static(publicPath));
-
+//rutas 
 app.use("/", Index)
 app.use("/producto", product)
 app.use("/carrito", product)
 app.use("/novedades", Novedades)
 app.use("/registro", registro)
 app.use("/login", registro)
-
+//levantar servidor
 app.listen(5000,() => {
     console.log ("Servidor corriendo en el puerto 5000");
  } );
