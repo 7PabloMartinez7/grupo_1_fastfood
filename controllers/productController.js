67479960e2081a268dbe42e948759772445accc0
gonzalotaritolay94@gmail.com <gonzalotaritolay94@gmail.com>
2023-09-04 04:08:08 -0300

    agregando js a ejs

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,10 @@
+const fs=require("fs")
+const path = require ("path")
+const productoJson = path.join(__dirname,"../data/products.json")
 const productController = {
     product: (req,res) => {
+        const product= JSON.parse(fs.readFileSync(productoJson, "utf-8"))
+        console.log(product[0])
         res.render ("productDetail")
     }
 }
