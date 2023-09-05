94a16fae3e88a5f60fc5cc11f7cbe545c5d232c9
gonzalotaritolay94@gmail.com <gonzalotaritolay94@gmail.com>
2023-09-05 00:33:14 -0300

    modificando rutas

--- routes/newProducto.js
+++ routes/newProducto.js
@@ -1,5 +1,6 @@
 const express= require("express");
 const router= express.Router();
 const newProductController= require("../controllers/newProductController.js");
+
 router.get("/agregarProducto",newProductController.newProduct);
 module.exports=router;
