9b4c409e01ccb621557f9ed5283150b3a21a884a
marcoscussi16 <marcoscussi16@gmail.com>
2023-09-27 11:04:30 -0300

    modif registro controller

--- controllers/registroController.js
+++ controllers/registroController.js
@@ -1,6 +1,25 @@
+const { json } = require("express")
+const fs=require("fs")
+const path = require ("path")
+const users = path.join(__dirname,"../data/users.json")
 const registroController = {
     registro: (req,res) => {
         res.render ("registro")
+    },
+    registroGuardado: (req, res) => {
+        const data = req.body
+        const usuario = JSON.parse(fs.readFileSync(users, "utf-8"))
+        const registroUsuario = {
+            id: usuario[usuario.length-1].id+1, 
+            nombre: data.nombre, 
+            apellido: data.apellido,
+            email: data.email,
+            contraseña: data.contrasenia,
+            avatar: req.file ? req.file.filename: "default-image.png"
+        }
+        usuario.push(registroUsuario)
+        fs.writeFileSync(users, JSON.stringify(usuario, null, " "))
+        res.send ("registro exitoso")
     }
 }
 module.exports = registroController;
--- routes/registro.js
+++ routes/registro.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require ("multer");
+const path = require ("path");
 const registroController = require("../controllers/registroController.js");
 router.get("/registro",registroController.registro); 
 //configurando multer para leer archivos
@@ -16,6 +17,6 @@ const storage = multer.diskStorage({
     },
 });
 const upload = multer ({storage: storage});
-router.post("/registro", upload.single("avatar"),registroController.registro);
+router.post("/registro", upload.single("avatar"),registroController.registroGuardado);
 
 module.exports=router;
