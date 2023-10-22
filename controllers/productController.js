afdce2242e97781c9d75c24a59d278eef53f2219
marcoscussi16 <marcoscussi16@gmail.com>
2023-10-21 23:30:24 -0300

    creando modelo usuario(falta relacionar)

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,15 @@
+const db = require("../database/models")
+
 const fs=require("fs")
 const path = require ("path")
 const productoJson = path.join(__dirname,"../data/products.json")
 const productController = {
     product: (req,res) => {
-        const product= JSON.parse(fs.readFileSync(productoJson, "utf-8"))
-        res.render ("product", {product:product})
+        //codigo para que lea el modelo Producto.js
+        db.Productos.findAll()
+        .then((productos) => {
+            res.send(productos)
+        })
     },
     detalle: (req,res) => {
         const product= JSON.parse(fs.readFileSync(productoJson, "utf-8"))
---
+++ database/models/Usuario.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "Usuarios";
+
+    let cols = {
+        id_User:{
+                    type: dataTypes.INTEGER,
+                    primaryKey: true,
+                    autoIncrement: true
+        },
+        nombre:{
+                type: dataTypes.STRING
+
+        },
+        apellido:{
+            type: dataTypes.STRING
+
+        },
+        email:{
+                    type: dataTypes.STRING
+        },
+        contrasenia:{
+                type: dataTypes.STRING
+        },
+        avatar:{
+                type: dataTypes.STRING
+        },
+    }
+
+    let config = {
+        tableName: "usuarios",
+        timestamps: false
+    }
+
+
+    const usuarios = sequelize.define(alias, cols, config);
+    return usuarios;
+}
