3a6e75fe78305c650b23c68b8c0ddb8f0c525fad
marcoscussi16 <marcoscussi16@gmail.com>
2023-11-27 01:39:31 -0300

    cambios en registro e index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,14 @@
 window.addEventListener("load", () =>{
 
-    let form = document.querySelector()
+    let form = document.querySelector(".form");
+    form.name.focus();
+
+    form.addEventListener("submit",(submitEvent)=> {
+
+        /*creacion de array de erroes vacio.
+        si se detectan errores se iran agregando aqui*/
+        let errors = [];
+
+        
+    })
 })
