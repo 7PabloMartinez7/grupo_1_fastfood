6778dcb90de3576c7f6fd498ddd5e154dd5f4f67
marcoscussi16 <marcoscussi16@gmail.com>
2023-12-11 11:46:29 -0300

    modificando appjs loginjs y loginejs

--- public/js/login.js
+++ public/js/login.js
@@ -26,37 +26,37 @@ window.addEventListener("load", () => {
             password.classList.remove("is-invalid");
         }
 
-        // Verificar credenciales en el backend (debes implementar este paso)
-        if (errors.length === 0) {
-            try {
-                const response = await fetch("/api/login", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        email: email.value,
-                        password: password.value,
-                    }),
-                });
+        // // Verificar credenciales en el backend (debes implementar este paso)
+        // if (errors.length === 0) {
+        //     try {
+        //         const response = await fetch("/api/login", {
+        //             method: "POST",
+        //             headers: {
+        //                 "Content-Type": "application/json",
+        //             },
+        //             body: JSON.stringify({
+        //                 email: email.value,
+        //                 password: password.value,
+        //             }),
+        //         });
 
-                if (!response.ok) {
-                    throw new Error("Error en la autenticación");
-                }
+        //         if (!response.ok) {
+        //             throw new Error("Error en la autenticación");
+        //         }
 
-                const data = await response.json();
+        //         const data = await response.json();
 
-                if (data.success) {
-                    alert("Inicio de sesión exitoso");
-                    // Redireccionar o realizar alguna acción después de un inicio de sesión exitoso
-                } else {
-                    errors.push("Credenciales incorrectas");
-                }
-            } catch (error) {
-                console.error(error);
-                errors.push("Error en la autenticación");
-            }
-        }
+        //         if (data.success) {
+        //             alert("Inicio de sesión exitoso");
+        //             // Redireccionar o realizar alguna acción después de un inicio de sesión exitoso
+        //         } else {
+        //             errors.push("Credenciales incorrectas");
+        //         }
+        //     } catch (error) {
+        //         console.error(error);
+        //         errors.push("Error en la autenticación");
+        //     }
+        // }
 
         // Mostrar errores
         if (errors.length > 0) {
